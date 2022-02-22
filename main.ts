f1b9df5840350d7b9a23fa9cf2dda16052dc3453
David Hall <david.lw.hall@gmail.com>
2022-02-21 22:00:41 -0800

    update meta information

--- main.ts
+++ main.ts
@@ -4,42 +4,31 @@ import AsyncSuggestionModal from "./AsyncSuggestionModal";
 
 const Mustache = require('mustache');
 
-interface ArxivGetterSettings {
+interface PaperClipperSettings {
 	paperTemplate?: string;
 	titleTemplate?: string;
 }
 
-const DEFAULT_SETTINGS: ArxivGetterSettings = {
+const DEFAULT_SETTINGS: PaperClipperSettings = {
 	paperTemplate: '',
 	titleTemplate: '{{id}}'
 }
 
-export default class ArxivGetter extends Plugin {
-	settings: ArxivGetterSettings;
+export default class PaperClipper extends Plugin {
+	settings: PaperClipperSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-arxiv-paper-getter',
+			id: 'open-paper-clipper',
 			name: 'Retrieve a paper by ID or URL',
 			callback: () => {
-				new ArxivGetterModal(this).open();
+				new PaperClipperModal(this).open();
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new ArxivGetterSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new PaperClipperSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -101,15 +90,15 @@ interface ArticleTemplateFields extends Article {
 }
 
 
-class ArxivGetterModal extends AsyncSuggestionModal<Article> {
+class PaperClipperModal extends AsyncSuggestionModal<Article> {
 
-	constructor(plugin: ArxivGetter) {
+	constructor(plugin: PaperClipper) {
 		super(plugin.app);
 		this.plugin = plugin;
 	}
 
 	articles?: Article[];
-	plugin: ArxivGetter;
+	plugin: PaperClipper;
 
 	onOpen() {
 		const {inputEl} = this;
@@ -148,10 +137,10 @@ class ArxivGetterModal extends AsyncSuggestionModal<Article> {
 	}
 }
 
-class ArxivGetterSettingTab extends PluginSettingTab {
-	plugin: ArxivGetter;
+class PaperClipperSettingTab extends PluginSettingTab {
+	plugin: PaperClipper;
 
-	constructor(app: App, plugin: ArxivGetter) {
+	constructor(app: App, plugin: PaperClipper) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -161,7 +150,7 @@ class ArxivGetterSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for ArXiv Getter.'});
+		containerEl.createEl('h2', {text: 'Settings for Paper Clipper.'});
 
 		new Setting(containerEl)
 			.setName('Paper Note Template')
